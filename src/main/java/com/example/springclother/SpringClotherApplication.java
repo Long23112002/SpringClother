fb2587bb4ed45e5e0eb98be86032899c7f5b97df
Long23112002 <115551484+Long23112002@users.noreply.github.com>
2024-02-20 19:59:24 +0700

    long comit

---
+++ src/main/java/com/example/springclother/SpringClotherApplication.java
@@ -0,0 +1,13 @@
+package com.example.springclother;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringClotherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringClotherApplication.class, args);
+    }
+
+}
